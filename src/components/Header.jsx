d4608b58165276fd1ee5ca6bac26d06d934b29ae
Hasty Alvin <quangthang0311@gmail.com>
2024-10-17 13:38:34 +0700

    Header DOne

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="bg-black  p-4     flex items-center justify-between">
+      <div className="Page flex items-center space-x-4">
+        <h1 className="text-red-700 text-[40px] uppercase font-bold">Movie</h1>
+        <nav className="flex items-center space-x-4">
+          <a className="text-white font-medium" href="">
+            Home
+          </a>
+          <a className="text-white font-medium" href="">
+            About
+          </a>
+          <a className="text-white font-medium" href="">
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="Search flex items-center space-x-4">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+          className="p-2 px-4 rounded-lg text-black"
+        />
+        <button
+          type="button"
+          className="bg-red-700 py-1 px-3 text-white font-semibold rounded-lg"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Chỉ định các file chứa JSX, TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
