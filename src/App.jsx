422e1f00de612ed6fb82c5d0240546789c0eec03
Hasty Alvin <quangthang0311@gmail.com>
2024-10-21 08:30:35 +0700

    Xong API

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Banner from "./components/Banner";
 import MovieList from "./components/MovieList";
 function App() {
   const [movie, setMovie] = useState();
-
-  console.log(typeof movie);
   
   useEffect(() => {
     const fetchMovie = async () => {
@@ -13,21 +11,17 @@ function App() {
         method: "GET",
         headers: {
           accept: "application/json",
-          Authorization: `Bearer ${import.meta.env.REACT_APP_API_KEY}`,
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
         },
       };
 
-      const url = [
-        "https://api.themoviedb.org/3/trending/movie/day?language=vi",
-        "https://api.themoviedb.org/3/movie/top_rated?language=vi",
-      ];
+      const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
       const response = await fetch(url, options);
       const data = await response.json();
       console.log(data);
     };
     fetchMovie();
   }, []);
-
   return (
     <div className="bg-black pb-10">
       <Header />
